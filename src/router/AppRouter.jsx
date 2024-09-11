a616274aefe53868e206f17f4c93f64c47588adb
jose99segura <jose99segura@gmail.com>
2024-09-11 21:35:32 +0200

    trying cursor IDE. leaning react routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,20 +1,19 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 
-import { MarvelPage } from "../heroes/pages/MarvelPage"
-import { DcPage } from "../heroes/pages/DcPage"
-import { LoginPage } from "../auth/pages/LoginPage"
+
+import { Navbar } from "../ui"
+import { HeroesRoutes } from "../heroes"
+import { LoginPage } from "../auth"
 
 
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-            <Route path="/marvel" element={<MarvelPage />} />
-            <Route path="/dc" element={<DcPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            +<Route path="/*" element={<Navigate to="marvel" />} />
-        </Routes>
+      <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/*" element={<HeroesRoutes />} />
+      </Routes>
     </>
   )
 }
