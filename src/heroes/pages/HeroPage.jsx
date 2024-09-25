5a83b04f08617d3f03707e385e06a0c568fa3357
jose99segura <jose99segura@gmail.com>
2024-09-25 23:09:58 +0200

    heroes page finished

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,12 +1,18 @@
-import { Navigate, useParams } from "react-router-dom"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
 import { getHeroById } from "../helpers";
+import { useMemo } from "react";
 
 
 export const HeroPage = () => {
 
   const {id} = useParams();
+  const navigate = useNavigate();
 
-  const hero = getHeroById( id );
+  const hero = useMemo( () => getHeroById( id ), [ id ] );
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   if( !hero ){
     <Navigate to="/marvel" />
@@ -14,7 +20,38 @@ export const HeroPage = () => {
 
   return (
    <>
-    <h1></h1>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img
+          src={`../assets/heroes/${id}.jpg`}
+          alt={ hero.superhero }
+          className="img-thumbnail animate__animated animate__fadeInLeft"
+        />
+      </div>
+
+      <div className="col-3">
+        <h3>
+          { hero.superhero }
+        </h3>
+        <ul className="list-group list-group-flush">
+          <li>
+            <b>Alter ego:</b> { hero.alter_ego }
+          </li>
+          <li>
+            <b>Publisher:</b> { hero.publisher }
+          </li>
+          <li>
+            <b>First appearance:</b> { hero.first_appearance }
+          </li>
+        </ul>
+          <h5 className="mt-3">Characters</h5>
+          <p>{ hero.characters }</p>
+          <button onClick={ onNavigateBack } className="btn btn-outline-primary">
+            Return
+          </button>
+      </div>
+
+    </div>
    </>
   )
 }
--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,8 +1,87 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import queryString from "query-string";
+import { HeroCard } from "../components"
+import { useForm } from "../../hooks/useForm";
+import { getHeroesByName } from "../helpers";
 
 
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse( location.search );
+
+  const heroes = getHeroesByName( q );
+
+  const { searchText, onInputChange } = useForm({
+    searchText: q
+  });
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    if ( searchText.trim().length <= 1 ) {
+      return;
+    }
+    navigate(`?q=${ searchText }`);
+  }
+
   return (
-    <div>SearchPage</div>
+    <>
+
+      <h1>Search Page</h1>
+
+      <div className="row">
+        <div className="col-5">
+          <h4>Search Form</h4>
+          <hr />
+
+          <form onSubmit={ onSearchSubmit }>
+            <input 
+              type="text"
+              placeholder="Find your hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={ searchText }
+              onChange={ onInputChange }
+            />
+
+            <button
+              type="submit"
+              className="btn m-1 btn-block btn-outline-primary"
+            >
+              Search...
+            </button>
+
+          </form>
+        </div>
+
+        <div className="col-7">
+
+          <h4>Results</h4>
+          <hr />
+
+          {
+            ( q === '' )
+            ? <div className="alert alert-info">Search a hero</div>
+            : (heroes.length === 0) &&
+            <div className="alert alert-danger">No results with { q }</div>
+          }
+
+
+            { 
+              heroes.map( hero => (
+                <div key={ hero.id }>
+                  <HeroCard key={ hero.id } { ...hero } />
+                </div>
+              ))
+            }
+
+        </div>
+      </div>
+    
+    </>
   )
 }
