5be95476db0b1eb9c5f5d29474267b19a0e7be4e
jose99segura <jose99segura@gmail.com>
2024-10-14 21:32:30 +0200

    route protection

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,23 +1,35 @@
-import { useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
 
+  const { login } = useContext( AuthContext );
   const navigate = useNavigate();
 
   const onLogin = () => {
-    navigate('/', {
+    
+    const lastPath = localStorage.getItem('lastPath') || '/';
+
+    login( 'Fernando Herrera' );
+    
+    navigate( lastPath, {
       replace: true
     });
   }
 
   return (
     <div className="container mt-5">
-        <h1>Login</h1>
-        <hr />
+      <h1>Login</h1>
+      <hr />
 
-        <button onClick={ onLogin } className="btn btn-primary">
-            Login
-        </button>
+      <button 
+        className="btn btn-primary"
+        onClick={ onLogin }
+      >
+        Login
+      </button>
+
     </div>
   )
 }
